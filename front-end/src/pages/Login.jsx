3e69b99b305df8e104ea74c05f53d0883740d5e1
Paulo Renan <able.paulorenan@gmail.com>
2022-04-27 18:56:59 -0300

    feat: req4

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -26,6 +27,12 @@ function Login() {
       setEmailError(true);
     } else {
       setEmailError(false);
+      axios.post('https://the-social-back.herokuapp.com/', {
+        email,
+        password,
+      }).catch(() => {
+        setEmailError(true);
+      });
     }
   };
 
