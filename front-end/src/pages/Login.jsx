b0e271ce8e5f53f5a3c41e439d9a766200a0ee1a
Paulo Renan <able.paulorenan@gmail.com>
2022-04-27 18:24:09 -0300

    feat: req2

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === '') {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <form>
+        <label htmlFor="email">
+          Login
+          <input
+            type="text"
+            data-testid="common_login__input-email"
+            name="email"
+            id="email"
+            value={ email }
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            data-testid="common_login__input-password"
+            name="password"
+            id="password"
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+        </label>
+        { emailError
+          && (
+            <p data-testid="common_login__element-invalid-email">
+              Email ou senha inválidos
+            </p>
+          )}
+        <button
+          type="submit"
+          data-testid="common_login__button-login"
+          onClick={ handleSubmit }
+        >
+          LOGIN
+        </button>
+      </form>
+      <button type="button" data-testid="common_login__button-register">
+        REGISTER
+      </button>
     </div>
   );
 }
