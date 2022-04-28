41219a64e433e62a7a7883f32bf99ee3c38b26a7
Isaac Matheus <isaacase@hotmail.com>
2022-04-27 22:22:03 -0300

    feat: configuração inicial dos errors Handlers

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(require('cors')());
+
+app.use(require('body-parser').json());
+
+app.use('/', require('../routes'));
+
+app.use('/', require('../handlers/errorHandler'));
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
