b36bcf14efa0c7cfd863f28d60ba07a7f0780882
Paulo Renan <able.paulorenan@gmail.com>
2022-04-27 22:36:36 -0300

    feat: redireciona para outra pagina quando o login é bem sucedido

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Products from './pages/Products';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
+      <Route path="/customer/products" element={ <Products /> } />
       <Route path="*" element={ <Navigate to="/login" /> } />
     </Routes>
   );
