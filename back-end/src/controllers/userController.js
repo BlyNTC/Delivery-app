63e7d6f34e6f15c9e8a09123c95efbf01c7dce37
Andrea <andrealh08@gmail.com>
2022-04-28 16:37:07 -0300

    fix: lint errors

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -4,7 +4,7 @@ const registerSchema = require('../schemas/registerSchema');
 const { validateWithJoi } = require('../utils/errors');
 const { status } = require('../utils/errorsMessages');
 
-const create = rescue(async(req, res) => {
+const create = rescue(async (req, res) => {
   validateWithJoi(registerSchema, req.body);
   const user = await Service.create(req.body);
   res.status(status.created).json(user);
