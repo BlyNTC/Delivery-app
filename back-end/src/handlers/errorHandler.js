aefe6c896b8abb7eed22e3acf44c5e28c639bcf6
Isaac Matheus <isaacase@hotmail.com>
2022-04-28 13:41:02 -0300

    feat: tira status hardcode, e tira alguns comentarios de rotas

--- back-end/src/handlers/errorHandler.js
+++ back-end/src/handlers/errorHandler.js
@@ -1,10 +1,11 @@
+const { status } = require('../utils/errorsMessages');
 const errorHandler = (err, _req, res, _next) => {
   let { code } = err;
-  if (err.message === 'invalid token' /* || err.message.includes('jwt') */) {
-    return res.status(401)
+  if (err.message === 'invalid token' || err.message.includes('jwt')) {
+    return res.status(status.unauthorized)
     .json({ message: 'Expired or invalid token' }); 
   }
-  if (!code) code = 500;
+  if (!code) code = status.internalServerError;
   console.log('ERROR HANDLER', err);
   res.status(code)
     .json({ message: err.message });
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,15 +1,9 @@
 const { Router } = require('express');
 
-// const user = require('./user');
 const login = require('./loginRouter');
-// const categories = require('./categories');
-// const post = require('./blogPost');
 
 const router = Router();
 
-// router.use('/user', user);
 router.use('/login', login);
-// router.use('/categories', categories);
-// router.use('/post', post);
 
 module.exports = router;
