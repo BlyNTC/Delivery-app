40311372deb2e8cffff5df0f7a1b17918cf37b10
Andrea <andrealh08@gmail.com>
2022-04-28 17:37:49 -0300

    update: muda o retorno da funcao create da userService

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,11 +1,21 @@
 const md5 = require('md5');
 const { User } = require('../database/models');
+const { jwtSign } = require('../utils/jwt');
 
 const create = async (body) => {
   const { name, email, password } = body;
-  const user = await User
-    .create({ name, email, password: md5(password), role: 'customer', raw: true });
-  if (user) return { message: 'Created' };
+  const role = 'customer';
+  const userData = await User
+    .create({ name, email, password: md5(password), role });
+  if (userData) {
+    const user = {
+      id: userData.dataValues.id,
+      name,
+      email,
+      role,
+    }
+    return { ...user, token: jwtSign(user, `${process.cwd()}/jwt.evaluation.key)`) };
+  }
 };
 
 module.exports = { create };
