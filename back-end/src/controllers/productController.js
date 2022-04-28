1839c4a5f0b9472c9b15fe8d58ccd4787885423f
Andrea <andrealh08@gmail.com>
2022-04-28 18:22:24 -0300

    feat: cria endpoint get /customer/products

---
+++ back-end/src/controllers/productController.js
@@ -0,0 +1,10 @@
+const rescue = require('express-rescue');
+const Service = require('../services/productService');
+const { status } = require('../utils/errorsMessages');
+
+const read = rescue(async (_req, res) => {;
+  const user = await Service.read();
+  res.status(status.ok).json(user);
+});
+
+module.exports = { read }
