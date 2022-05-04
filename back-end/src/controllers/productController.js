e71d6b360c5f3e09f47a9e5a7b6315052d901edf
Lys Prestes <lysprestes@gmail.com>
2022-05-04 17:24:01 -0400

    feat: tests product controlles

--- back-end/src/controllers/productController.js
+++ back-end/src/controllers/productController.js
@@ -2,9 +2,16 @@ const rescue = require('express-rescue');
 const Service = require('../services/productService');
 const { status } = require('../utils/errorsMessages');
 
-const read = rescue(async (_req, res) => {
-  const user = await Service.read();
-  res.status(status.ok).json(user);
-});
+const read = rescue(
+  async (_req, res) => {
+    const user = await Service.read();
+    return res.status(status.ok).json(user);
+  },
+);
+
+// const read = async (_req, res) => {
+//     const user = await Service.read();
+//     return res.status(status.ok).json(user);
+// };
 
 module.exports = { read };
