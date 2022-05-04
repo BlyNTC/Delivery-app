78bd12ab110cba0e94a7dec4330cc62d478facb4
Andrea <andrealh08@gmail.com>
2022-05-03 22:33:21 -0300

    feat: adiciona funcao create basica para sale service e controller e o endpoint

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -2,9 +2,14 @@ const rescue = require('express-rescue');
 const saleService = require('../services/saleService');
 const { status } = require('../utils/errorsMessages');
 
+const create = async (req, res) => {
+  const criar = await saleService.create(req.body);
+  res.status(status.created).json(criar);
+};
+
 const read = async (_req, res) => {
   const response = await saleService.read();
   res.status(status.ok).json(response);
 };
 
-module.exports = { read };
+module.exports = { read, create };
--- back-end/src/services/saleService.js
+++ back-end/src/services/saleService.js
@@ -1,8 +1,13 @@
 const { Sale } = require('../database/models');
 
+const create = async(body) => {
+  const newSale = await Sale.create(body);
+  return newSale.dataValues;
+};
+
 const read = async () => {
   const readSale = await Sale.findAll();
   return readSale;
 };
 
-module.exports = { read };
+module.exports = { read, create };
