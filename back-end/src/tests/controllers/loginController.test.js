546c46fa60bfffec1af3527c02f1fb9a32f59acd
Lys Prestes <lysprestes@gmail.com>
2022-05-04 18:58:58 -0400

    fix: login controller tests

--- back-end/src/tests/controllers/loginController.test.js
+++ back-end/src/tests/controllers/loginController.test.js
@@ -1,37 +1,40 @@
-const Sinon = require('sinon');
-const { expect, request } = require('chai');
-const loginService = require('../../services/loginService');
-const { loginController }= require('../../controllers/loginController');
-const rescue = require('express-rescue');
-const { status } = require('../../utils/errorsMessages');
+const { expect } = require("chai");
+const Sinon = require("sinon");
+const loginController = require("../../controllers/loginController");
+const loginService = require("../../services/loginService");
+const { status } = require("../../utils/errorsMessages");
 
 const userMock = {
   id: 1,
-  email: 'teste@teste.com',
-  name: 'teste',
-  role: 'customer',
+  email: "teste@teste.com",
+  name: "teste",
+  role: "customer",
 };
 
-describe('LoginController', () => {
+describe("LoginController", () => {
   const response = {};
   const request = {};
-  describe('With valid user', () => {
-    before(() => {
-      Sinon.stub(loginService, 'loginService').resolves(userMock);
-      request.body = {
-        email: 'teste@teste.com',
-        password: '123456',
-      };
-      response.status = Sinon.stub().returns(response);
-      response.json = Sinon.stub().returns(userMock);
-    });
-    after(() => {
-      loginService.loginService.restore();
-    });
-    it('should return user', async () => {
-      await loginController(rescue(request, response));
-      expect(response.status.calledWith(status.ok)).to.be.equal(true);
-      expect(response.json.calledWith(userMock))
+  const next = Sinon.spy();
+
+  describe("With valid user,", () => {
+    describe("when succesful,", () => {
+      before(() => {
+        Sinon.stub(loginService, "loginService").resolves(userMock);
+        request.body = {
+          email: "teste@teste.com",
+          password: "123456",
+        };
+        response.status = Sinon.stub().returns(response);
+        response.json = Sinon.stub().returns();
+      });
+      after(() => {
+        loginService.loginService.restore();
+      });
+      it("should return status 200", async () => {
+        await loginController.loginController(request, response, next);
+        expect(response.status.calledWith(200)).to.be.equal(true);
+        expect(response.json.calledWith(userMock));
+      });
     });
   });
 });
