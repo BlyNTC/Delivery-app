59842fe2f65a79646f989c9f642667c941049088
Isaac Matheus <isaacase@hotmail.com>
2022-05-04 22:49:21 -0300

    fix: data-testid's digitados de maneira incorreta

--- front-end/src/components/CartItem.jsx
+++ front-end/src/components/CartItem.jsx
@@ -4,25 +4,30 @@ import PropTypes from 'prop-types';
 function CartItem({ product, index, onClick }) {
   return (
     <tr>
-      <td>{index + 1}</td>
       <td
-        data-test-id={ `customer_checkout__element-order-table-name-${index}` }
+        data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
+      >
+        {index + 1}
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-name-${index}` }
+        //              customer_checkout__element-order-table-name-
       >
         {product.name}
 
       </td>
       <td
-        data-test-id={ `customer_checkout__element-order-table-quantity-${index}` }
+        data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
       >
         {product.qty}
       </td>
       <td
-        data-test-id={ `customer_checkout__element-order-table-unit-price-${index}` }
+        data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
       >
         {Number(product.price).toFixed(2).replace('.', ',')}
       </td>
       <td
-        data-test-id={ `customer_checkout__element-order-table-sub-total-${index}` }
+        data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
       >
         {(product.qty * product.price).toFixed(2).replace('.', ',')}
       </td>
@@ -30,7 +35,7 @@ function CartItem({ product, index, onClick }) {
         <button
           type="button"
           onClick={ () => onClick(product.id) }
-          data-test-id={ `customer_checkout__element-order-table-remove-${index}` }
+          data-testid={ `customer_checkout__element-order-table-remove-${index}` }
         >
           Remover
 
