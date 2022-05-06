19cee3f34f546cc686ec6f01c41a808040cbb3a8
Lys Prestes <lysprestes@gmail.com>
2022-05-06 16:05:35 -0400

    feat: saleOrder component

---
+++ front-end/src/pages/SellerOrders.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { getOrders } from '../utils/axios';
+
+import { OrderCard, Header } from '../components';
+
+export default function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    getOrders().then((data) => {
+      const getLocalStorage = JSON.parse(localStorage.getItem('user'));
+      const sales = data.filter((res) => res.userId === getLocalStorage.id);
+      setOrders(sales);
+    }).catch(() => setOrders([]));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      { orders.map((order, index) => (
+        <OrderCard
+          saleId={ order.id }
+          status={ order.status }
+          date={ order.dateSale }
+          totalPrice={ order.totalPrice }
+          address={ order.address }
+          key={ index }
+        />
+      )) }
+    </div>
+  );
+}
