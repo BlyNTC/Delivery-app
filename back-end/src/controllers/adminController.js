bc1927701b73a76993b456ef5a4487f529a41a1e
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2022-05-06 18:30:08 -0300

    feat: inserindo verificador antes de ser criado

--- back-end/src/controllers/adminController.js
+++ back-end/src/controllers/adminController.js
@@ -1,10 +1,14 @@
 const rescue = require('express-rescue');
 const Service = require('../services/adminService');
-const { status } = require('../utils/errorsMessages');
+const { status, messages } = require('../utils/errorsMessages');
 
 const create = rescue(async (req, res) => {
+  const verificacao = await Service.verify(req.body);
   const user = await Service.create(req.body);
-  res.status(status.created).json(user);
+  if(verificacao) {
+    return res.status(status.conflict).json(messages.USER_REGISTERED)
+  }
+  return res.status(status.created).json(user);
 });
 
 module.exports = { create };
